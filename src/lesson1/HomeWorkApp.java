9ddc5e34fe9e5863b901206d66a08f64067103e0
Foolfire <cisternsystem@gmail.com>
2021-11-16 18:07:23 +0300

    making changes in the develop branch

--- src/lesson1/HomeWorkApp.java
+++ src/lesson1/HomeWorkApp.java
@@ -28,8 +28,42 @@ package lesson1;
 
 public class HomeWorkApp
 {
+    static void printThreeWords()
+    {
+        for (String s : new String[] { "Orange", "Banana", "Apple" })
+        {
+            System.out.println(s);
+        }
+    }
+
+    static void checkSumSign(int a, int b)
+    {
+        System.out.println("Сумма " + (a + b < 0 ? "отрицательная" : "положительная"));
+    }
+
+    static void printColor()
+    {
+        int value = (int)Math.round(Math.random() * 200);
+        if (Math.random() * 100 < 50) value *= -1;
+        System.out.print(value + " - ");
+        if (value <= 0)
+            System.out.println("Красный");
+        else if (value <= 100)
+            System.out.println("Желтый");
+        else
+            System.out.println("Зеленый");
+    }
+
+    static void compareNumbers(int a, int b)
+    {
+        System.out.println(a < b ? "a < b" : "a >= b");
+    }
+
     public static void main(String[] args)
     {
-
+        printThreeWords();
+        checkSumSign(-5, 3);
+        printColor();
+        compareNumbers(-7, -2);
     }
 }
